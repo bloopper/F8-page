be9781090b3fbea47bea39af4b8cc990428550fc
Hòa Vũ <vuminhhoa.2002@gmail.com>
2022-02-19 16:40:59 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,47 @@
+'use strict';
+(function eventHandler() {
+    const $ = document.querySelector.bind(document);
+    const popin = $('.test');
+    const button = $('.sidebar_button-circle');
+    let active = false;
+
+    // rotating button
+    button.onclick = function() {
+        // add animation class when clicked 
+        if( active == false ) {
+            this.classList.add('no-hover');
+            this.classList.add('sidebar_button-active');
+            popin.innerHTML = `<div class="sidebar_menu-module animation_drop-in" >
+                                <ul>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa-solid fa-pen"></i>
+                                            <span> Viết blog </span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>`;
+            active = true;
+        } 
+        // remove animation class when cliked again
+        else { 
+            const sidebarMenu = $('.sidebar_menu-module')
+            this.classList.remove('no-hover');
+            this.classList.remove('sidebar_button-active');
+            sidebarMenu.classList.remove('animation_drop-in');
+            sidebarMenu.classList.add('animation_fade-out');
+
+            setTimeout(function() {
+                sidebarMenu.remove();
+            }, 300)
+
+            active = false;
+        } 
+
+        
+
+        // Ignor this line
+        // For runtime error because i didn't provide a promise
+        return Promise.resolve("Dummy response to keep the console quiet");
+    }
+})(); 
